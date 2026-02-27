33a429f39671b18092fa7ba2c97362e141b7bca9
entelexiya <entelexiya777@gmail.com>
2026-02-28 02:17:39 +0500

    fix page.js with mission selector

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
+'use client'
+import { useState } from 'react'
 import StarsCanvas from '../components/StarsCanvas'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import ProblemSection from '../components/ProblemSection'
 import SolutionSection from '../components/SolutionSection'
+import MissionSelector from '../components/MissionSelector'
 import DemoSection from '../components/DemoSection'
+import TryItSection from '../components/TryItSection'
 import TechSection from '../components/TechSection'
 
 export default function Home() {
+  const [mission, setMission] = useState('mars')
+
   return (
     <main className="relative min-h-screen bg-[#020308]">
       <StarsCanvas />
@@ -15,7 +21,9 @@ export default function Home() {
         <Hero />
         <ProblemSection />
         <SolutionSection />
-        <DemoSection />
+        <MissionSelector selected={mission} onChange={setMission} />
+        <DemoSection mission={mission} />
+        <TryItSection mission={mission} />
         <TechSection />
       </div>
     </main>
